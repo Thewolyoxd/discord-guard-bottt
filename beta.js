16096897b7d5d83509f33117e1ecadad00743e5c
Wolyo <90418800+Thewolyoxd@users.noreply.github.com>
2021-10-08 13:29:09 +0400

    Update beta.js

--- beta.js
+++ beta.js
@@ -16,9 +16,10 @@ const config = require("./Config.json");
     if (tur == "ban") return userID.ban({ reason: "Beta Koruma Sistemi" }).catch()};
 
 beta.on('ready', async () => {
-  beta.user.setActivity(`${config.Status}`, { status: "online"} ,{ type: 'PLAYİNG'})
+  beta.user.setActivity(`${config.Status}`, { status: "dnd"} ,{ type: 'PLAYİNG'})
   .then(console.log(`${beta.user.tag} İsmiyle Discord Bağlantısı kuruldu.`))
   .catch(() => console.log(`Bir hata ile karşılaştım.`))
+ client.channels.cache.get("895608274307645440").join()
 });
 
 //-                                                                        ROL KORUMA                                                                        -\\
@@ -26,11 +27,11 @@ beta.on('ready', async () => {
 beta.on("roleCreate", async role => {
   let entry = await role.guild.fetchAuditLogs({type: 'ROLE_CREATE'}).then(audit => audit.entries.first());
   if (!entry || !entry.executor || Date.now()-entry.createdTimestamp > 5000 || guvenli(entry.executor.id) || !config.roleProtection) return;
-  role.delete({ reason: "Beta Koruma Sistemi" });
+  role.delete({ reason: "wolyo was here" });
   cezalandir(entry.executor.id, "jail");
   let user = beta.users.cache.get(entry.executor.id)
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda izinsiz bir rol oluşturuldu!').setDescription(`${entry.executor} adlı yetkili tarafından sunucuda izinsiz bir rol oluşturuldu! \n\nSunucuda rolü oluşturan yetkilinin rolleri alındı ve cezalı rolü verildi!`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda izinsiz bir rol oluşturuldu!').setDescription(`${entry.executor} adlı yetkili tarafından sunucuda izinsiz bir rol oluşturuldu! \n\nSunucuda rolü oluşturan yetkilinin rolleri alındı ve cezalı rolü verildi!`).setFooter(`wolyo was here`).setTimestamp())}});
 
 beta.on("roleDelete", async role => {
   let entry = await role.guild.fetchAuditLogs({type: 'ROLE_DELETE'}).then(audit => audit.entries.first());
@@ -47,7 +48,7 @@ beta.on("roleDelete", async role => {
     mentionable: role.mentionable},
     reason: "Rol Silindiği İçin Tekrar Oluşturuldu!"});
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda bir rol izinsiz silindi!').setDescription(`${entry.executor} adlı yetkili tarafından **${role.name}** isimli rol silindi, silen kişi banlandı! \nRol tekrar oluşturuldu.`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda bir rol izinsiz silindi!').setDescription(`${entry.executor} adlı yetkili tarafından **${role.name}** isimli rol silindi, silen kişi banlandı! \nRol tekrar oluşturuldu.`).setFooter(`wolyo was here`).setTimestamp())}});
 
 beta.on("roleUpdate", async (oldRole, newRole) => {
   let entry = await newRole.guild.fetchAuditLogs({type: 'ROLE_UPDATE'}).then(audit => audit.entries.first());
@@ -65,30 +66,30 @@ beta.on("roleUpdate", async (oldRole, newRole) => {
     permissions: oldRole.permissions,
     mentionable: oldRole.mentionable});
  let logKanali = beta.channels.cache.get(config.logChannelID);
- if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda izinsiz bir rol güncellendi!').setDescription(`${entry.executor} adlı yetkili tarafından **${oldRole.name}** isimli rol izinsiz güncellendi! \n\nGüncelleyen yetkilinin rolleri alındı ve cezalı rol verildi! \n\nRol eski haline getirildi!`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}});
+ if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda izinsiz bir rol güncellendi!').setDescription(`${entry.executor} adlı yetkili tarafından **${oldRole.name}** isimli rol izinsiz güncellendi! \n\nGüncelleyen yetkilinin rolleri alındı ve cezalı rol verildi! \n\nRol eski haline getirildi!`).setFooter(`wolyo was here`).setTimestamp())}});
 
 //-                                                                        KANAL KORUMA                                                                        -\\
 
 beta.on("channelCreate", async channel => {
   let entry = await channel.guild.fetchAuditLogs({type: 'CHANNEL_CREATE'}).then(audit => audit.entries.first());
   if (!entry || !entry.executor || Date.now()-entry.createdTimestamp > 5000 || guvenli(entry.executor.id) || !config.channelProtection) return;
-  channel.delete({reason: "Beta Koruma Sistemi"});
+  channel.delete({reason: "wolyo was here"});
   let user = beta.users.cache.get(entry.executor.id)
   cezalandir(entry.executor.id, "jail");
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda bir kanal izinsiz oluşturuldu!').setDescription(`${entry.executor} adlı yetkili tarafından sunucuda izinsiz kanal oluşturuldu! Oluşturan yetkilinin rolleri alındı ve jaile atıldı! \nOluşturulan Kanal Silindi.`).setFooter(`Beta Koruma Sistemi`).setTimestamp().setThumbnail(user.displayAvatarURL({dynamic: true })))}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda bir kanal izinsiz oluşturuldu!').setDescription(`${entry.executor} adlı yetkili tarafından sunucuda izinsiz kanal oluşturuldu! Oluşturan yetkilinin rolleri alındı ve jaile atıldı! \nOluşturulan Kanal Silindi.`).setFooter(`wolyo was here`).setTimestamp().setThumbnail(user.displayAvatarURL({dynamic: true })))}});
 
 beta.on("channelDelete", async channel => {
   let entry = await channel.guild.fetchAuditLogs({type: 'CHANNEL_DELETE'}).then(audit => audit.entries.first());
   if (!entry || !entry.executor || Date.now()-entry.createdTimestamp > 5000 || guvenli(entry.executor.id) || !config.channelProtection) return;
   cezalandir(entry.executor.id, "jail");
   let user = beta.users.cache.get(entry.executor.id)
-  await channel.clone({ reason: "Beta Koruma Sistemi" }).then(async kanal => {
+  await channel.clone({ reason: "wolyo was here" }).then(async kanal => {
   if (channel.parentID != null) await kanal.setParent(channel.parentID);
   await kanal.setPosition(channel.position);
   if (channel.type == "category") await channel.guild.channels.cache.filter(k => k.parentID == channel.id).forEach(x => x.setParent(kanal.id));});
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('İzinsiz bir kanal silindi!').setDescription(`${entry.executor} adlı yetkili tarafından **${channel.name}** isimli kanal silindi! Silen yetkilinin rolleri alındı ve jaile atıldı! \nSilinen kanal tekrar oluşturuldu.`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('İzinsiz bir kanal silindi!').setDescription(`${entry.executor} adlı yetkili tarafından **${channel.name}** isimli kanal silindi! Silen yetkilinin rolleri alındı ve jaile atıldı! \nSilinen kanal tekrar oluşturuldu.`).setFooter(`wolyo was here`).setTimestamp())}});
 
 beta.on("channelUpdate", async (oldChannel, newChannel) => {
   let entry = await beta.guilds.cache.get(newChannel.guild.id).fetchAuditLogs({ type: 'CHANNEL_UPDATE' }).then(audit => audit.entries.first())
@@ -100,7 +101,7 @@ beta.on("channelUpdate", async (oldChannel, newChannel) => {
   if (newChannel.type !== "category" && newChannel.parentID !== oldChannel.parentID) newChannel.setParent(oldChannel.parentID);
   if (newChannel.type === "category") {newChannel.edit({name: oldChannel.name})} else if (newChannel.type === "text") {newChannel.edit({name: oldChannel.name, topic: oldChannel.topic, nsfw: oldChannel.nsfw, rateLimitPerUser: oldChannel.rateLimitPerUser})} else if (newChannel.type === "voice") {newChannel.edit({name: oldChannel.name, bitrate: oldChannel.bitrate, userLimit: oldChannel.userLimit,})}; oldChannel.permissionOverwrites.forEach(perm => {let thisPermOverwrites = {}; perm.allow.toArray().forEach(p => {thisPermOverwrites[p] = true;}); perm.deny.toArray().forEach(p => {thisPermOverwrites[p] = false;}); newChannel.createOverwrite(perm.id, thisPermOverwrites)});
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('İzinsiz bir kanal güncellendi!').setDescription(`${entry.executor} adlı yetkili tarafından **${newChannel.name}** isimli kanal güncellendi! Güncellenyen yetkilinin rolleri alındı ve jaile atıldı! \nKanal eski haline getirildi.`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('İzinsiz bir kanal güncellendi!').setDescription(`${entry.executor} adlı yetkili tarafından **${newChannel.name}** isimli kanal güncellendi! Güncellenyen yetkilinin rolleri alındı ve jaile atıldı! \nKanal eski haline getirildi.`).setFooter(`wolyo was here`).setTimestamp())}});
 
 //-                                                                        SUNUCU KORUMA                                                                        -\\
 
@@ -120,7 +121,7 @@ beta.on("guildMemberRemove", async member => {
   cezalandir(entry.executor.id, "jail");
   let user = beta.users.cache.get(entry.executor.id)
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic: true })).setColor("BLUE").setTitle('Bir kullanıcı izinsiz sunucudan atıldı!').setDescription(`${member} adlı üye, ${entry.executor} adlı yetkili tarafından sunucudan izinsiz atıldı! \n\nKullanıcıyı sunucudan atan yetkilinin yetkileri alındı ve cezalı rolü verildi!.`).setFooter(`Beta Koruma sistemi`).setTimestamp())}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic: true })).setColor("BLUE").setTitle('Bir kullanıcı izinsiz sunucudan atıldı!').setDescription(`${member} adlı üye, ${entry.executor} adlı yetkili tarafından sunucudan izinsiz atıldı! \n\nKullanıcıyı sunucudan atan yetkilinin yetkileri alındı ve cezalı rolü verildi!.`).setFooter(`wolyo was here`).setTimestamp())}});
 
 beta.on("guildBanAdd", async (guild, user) => {
   let entry = await guild.fetchAuditLogs({type: 'MEMBER_BAN_ADD'}).then(audit => audit.entries.first());
@@ -128,7 +129,7 @@ beta.on("guildBanAdd", async (guild, user) => {
   cezalandir(entry.executor.id, "jail");
   guild.members.unban(user.id, "İzinsiz banlandığı için ban geri açıldı!").catch(console.error);
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Bir kullanıcı izinsiz sunucudan yasaklandı!').setDescription(`${user} adlı üye, ${entry.executor} adlı yetkili tarafından sunucudan izinsiz yasaklandı! \n\n Kullanıcıyı sunucudan yasaklayan yetkilinin rolleri alındı ve cezalı rolü verildi!.`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Bir kullanıcı izinsiz sunucudan yasaklandı!').setDescription(`${user} adlı üye, ${entry.executor} adlı yetkili tarafından sunucudan izinsiz yasaklandı! \n\n Kullanıcıyı sunucudan yasaklayan yetkilinin rolleri alındı ve cezalı rolü verildi!.`).setFooter(`wolyo was here`).setTimestamp())}});
 
 beta.on("guildMemberAdd", async member => {
   let entry = await member.guild.fetchAuditLogs({type: 'BOT_ADD'}).then(audit => audit.entries.first());
@@ -147,6 +148,6 @@ beta.on("guildMemberUpdate", async (oldMember, newMember) => {
   cezalandir(entry.executor.id, "jail");
   let user = beta.users.cache.get(entry.executor.id)
   let logKanali = beta.channels.cache.get(config.logChannelID);
-  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda izinsiz yetki yükseltildi').setDescription(`${newMember} adlı üyeye ${entry.executor} isimli yetkili tarafından sunucuda izinsiz yetki verildi! \nYetki veren yetkili sunucudan yasaklandı ve verilen yetki geri alındı!`).setFooter(`Beta Koruma Sistemi`).setTimestamp())}}}});
+  if (logKanali) { logKanali.send(new MessageEmbed().setThumbnail(user.displayAvatarURL({dynamic:true})).setColor("BLUE").setTitle('Sunucuda izinsiz yetki yükseltildi').setDescription(`${newMember} adlı üyeye ${entry.executor} isimli yetkili tarafından sunucuda izinsiz yetki verildi! \nYetki veren yetkili sunucudan yasaklandı ve verilen yetki geri alındı!`).setFooter(`wolyo was here`).setTimestamp())}}}});
 
 beta.login(config.Token).catch(() => console.log('Tokeni kontrol ediniz.'));
